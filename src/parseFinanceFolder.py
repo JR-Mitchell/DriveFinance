26fe8026a97aa7c077a2f9ed650f02777be7af6c
JrTheDragon01 <www.aereregion.webs.com@live.co.uk>
2020-07-02 15:42:47 +0100

    moved code into two parsing functions

--- src/parseFinanceFolder.py
+++ src/parseFinanceFolder.py
@@ -36,69 +36,25 @@ class InputPaymentData(InputFileReader):
         super(InputPaymentData,self).__init__(child_file)
         #Setting up variables for this initialisation that will have values set to them
         self.additional_text = ""
-        timestamp = None
-        timestampindex = None
-        time_and_date_stamp = None
-        date_stamp = None
-        send_bool = False
+        self._time_lock_object = {
+            "lastCalculationTime":None,
+            "indexOfLastCalculationTimestamp":None,
+            "lastCalculationDateTime":None,
+            "dateNow":None
+        }
+        self.send_bool = False
         control_indices = []
         #Passing through the lines and parsing them one by one
         for index, line in enumerate(self.lines):
-            if send_bool:
+            if self.send_bool: #Ensures only lines before the send token are processed for the current run and removed from the drive-facing file
                 control_indices.insert(0,index)
                 self.additional_text += line
                 self.additional_text += "\n"
             elif line[0] == "[": #a control tag
                 control_indices.insert(0,index)
-                if "timestamp of last calculation:" in line:
-                    if timestamp is not None:
-                        raise Exception("Timestamp defined multiple times!")
-                    timestamp = line.strip("[]").replace("timestamp of last calculation:","").strip()
-                    timestamp = timestamp.replace(".",":").split(":")
-                    timestamp = zip(("hour","minute","second","microsecond"),[int(item) for item in timestamp])
-                    timestampindex = index
-                elif "datenow:" in line:
-                    date_stamp = line.strip("[]").replace("datenow:","").strip().split("/")
-                    date_stamp = zip(("day","month","year"),[int(item) for item in date_stamp])
-                    if time_and_date_stamp is None:
-                        if timestamp is None: raise Exception("No valid timestamp found before first datenow!")
-                        time_and_date_stamp = dict(list(date_stamp)+list(timestamp))
-                        time_and_date_stamp["year"] += 2000
-                        time_and_date_stamp = pd.Timestamp(**time_and_date_stamp)
-                    date_stamp = dict(list(date_stamp)+list(DEFAULT_TIME))
-                    date_stamp["year"] += 2000
-                    date_stamp = pd.Timestamp(**date_stamp)
-                elif line.lower() == "[send]":
-                    send_bool = True
-                    self.additional_text += "[datenow: {}]\n".format(date_stamp.strftime("%d/%m/%y"))
-                else:
-                    raise Exception("No valid format for the line '{}'".format(line))
+                self.parse_control_tag(line,index)
             else:
-                purchase = re.search(r"^(?:spent\s)?(£?\d+(?:.\d\d)?)(?:\sspent)?\son\s(.+?)(?:(?:\spaid)?\s(?:by|from|using)(\s.+?)?)?$",line.strip())
-                if purchase:
-                    #Purchase. Format: {amount} (spent) on {object} (paid) (by|from|using) {payment method}
-                    amountSpent = float(purchase.group(1).strip("£"))
-                    spentOn = purchase.group(2).strip()
-                    payMethod = "__default_payment"
-                    if purchase.group(3) is not None:
-                        payMethod = purchase.group(3).strip()
-                    self.lines[index] = [amountSpent,payMethod,spentOn,time_and_date_stamp,date_stamp,"purchase"]
-                else:
-                    transfer = re.search(r"^(£?\d+(?:.\d\d)?)(?:(?:\stransferred|\staken(?:\sout)?)?\sfrom\s(.+?))?(?:(?:\stransferred)?\sto\s(.+?))?(\staken\sout)?$",line.strip())
-                    if transfer:
-                        #Transfer. Format: {amount} (transferred|taken (out)) from {account} (transferred) to {target}
-                        amountTransferred = float(transfer.group(1).strip("£"))
-                        if transfer.group(2) is None and transfer.group(3) is None and transfer.group(4) is None:
-                            raise Exception("No valid format for the line '{}'".format(line))
-                        outAccount = "__default_from_account"
-                        inAccount = "__default_to_account"
-                        if transfer.group(2) is not None:
-                            outAccount = transfer.group(2).strip()
-                        if transfer.group(3) is not None:
-                            inAccount = transfer.group(3).strip()
-                        self.lines[index] = [amountTransferred,outAccount,inAccount,time_and_date_stamp,date_stamp,"transfer"]
-                    else:
-                        raise Exception("No valid format for the line '{}'".format(line))
+                self.parse_payment_line(line,index)
         #getting rid of the "control indices"
         for index in control_indices:
             del self.lines[index]
@@ -106,8 +62,60 @@ class InputPaymentData(InputFileReader):
         finance_grid = zip(*self.lines)
         finance_grid = zip(("amount","from","to","id_time","date_made","type"),finance_grid)
         self.read_payments = pd.DataFrame(OrderedDict(finance_grid))
-        self.timestamp = time_and_date_stamp
-        self.send_bool = send_bool
+        self.timestamp = self._time_lock_object["lastCalculationDateTime"]
+
+    def parse_control_tag(self,line,index):
+        if "timestamp of last calculation:" in line: #modifies timestamp, timestampindex
+            if self._time_lock_object["lastCalculationTime"] is not None:
+                raise Exception("Timestamp defined multiple times!")
+            self._time_lock_object["lastCalculationTime"] = line.strip("[]").replace("timestamp of last calculation:","").strip()
+            self._time_lock_object["lastCalculationTime"] = self._time_lock_object["lastCalculationTime"].replace(".",":").split(":")
+            self._time_lock_object["lastCalculationTime"] = zip(("hour","minute","second","microsecond"),[int(item) for item in self._time_lock_object["lastCalculationTime"]])
+            self._time_lock_object["indexOfLastCalculationTimestamp"] = index
+        elif "datenow:" in line: #modifies date_stamp, time_and_date_stamp
+            self._time_lock_object["dateNow"] = line.strip("[]").replace("datenow:","").strip().split("/")
+            self._time_lock_object["dateNow"] = zip(("day","month","year"),[int(item) for item in self._time_lock_object["dateNow"]])
+            if self._time_lock_object["lastCalculationDateTime"] is None:
+                if self._time_lock_object["lastCalculationTime"] is None: raise Exception("No valid timestamp found before first datenow!")
+                self._time_lock_object["lastCalculationDateTime"] = dict(list(self._time_lock_object["dateNow"])+list(self._time_lock_object["lastCalculationTime"]))
+                self._time_lock_object["lastCalculationDateTime"]["year"] += 2000
+                self._time_lock_object["lastCalculationDateTime"] = pd.Timestamp(**self._time_lock_object["lastCalculationDateTime"])
+            self._time_lock_object["dateNow"] = dict(list(self._time_lock_object["dateNow"])+list(DEFAULT_TIME))
+            self._time_lock_object["dateNow"]["year"] += 2000
+            self._time_lock_object["dateNow"] = pd.Timestamp(**self._time_lock_object["dateNow"])
+        elif line.lower() == "[send]": #modifies self.send_bool, additional_text
+            self.send_bool = True
+            self.additional_text += "[datenow: {}]\n".format(self._time_lock_object["dateNow"].strftime("%d/%m/%y"))
+        else:
+            raise Exception("No valid format for the line '{}'".format(line))
+
+    def parse_payment_line(self,line,index):
+        purchase = re.search(r"^(?:spent\s)?(£?\d+(?:.\d\d)?)(?:\sspent)?\son\s(.+?)(?:(?:\spaid)?\s(?:by|from|using)(\s.+?)?)?$",line.strip())
+        if purchase:
+            #Purchase. Format: {amount} (spent) on {object} (paid) (by|from|using) {payment method}
+            amountSpent = float(purchase.group(1).strip("£"))
+            spentOn = purchase.group(2).strip()
+            payMethod = "__default_payment"
+            if purchase.group(3) is not None:
+                payMethod = purchase.group(3).strip()
+            self.lines[index] = [amountSpent,payMethod,spentOn,self._time_lock_object["lastCalculationDateTime"],self._time_lock_object["dateNow"],"purchase"]
+        else:
+            transfer = re.search(r"^(£?\d+(?:.\d\d)?)(?:(?:\stransferred|\staken(?:\sout)?)?\sfrom\s(.+?))?(?:(?:\stransferred)?\sto\s(.+?))?(\staken\sout)?$",line.strip())
+            if transfer:
+                #Transfer. Format: {amount} (transferred|taken (out)) from {account} (transferred) to {target}
+                amountTransferred = float(transfer.group(1).strip("£"))
+                if transfer.group(2) is None and transfer.group(3) is None and transfer.group(4) is None:
+                    raise Exception("No valid format for the line '{}'".format(line))
+                outAccount = "__default_from_account"
+                inAccount = "__default_to_account"
+                if transfer.group(2) is not None:
+                    outAccount = transfer.group(2).strip()
+                if transfer.group(3) is not None:
+                    inAccount = transfer.group(3).strip()
+                self.lines[index] = [amountTransferred,outAccount,inAccount,self._time_lock_object["lastCalculationDateTime"],self._time_lock_object["dateNow"],"transfer"]
+            else:
+                raise Exception("No valid format for the line '{}'".format(line))
+
 
 class ParsedFinanceFolder(object):
     def __init__(self,folder_name):
