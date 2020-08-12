0a998a66c84c1eb999cc498ddb12d8c1d8e672a7
JrTheDragon01 <www.aereregion.webs.com@live.co.uk>
2020-08-12 11:49:24 +0100

    Added --report, --setup (currently nonfunctional) arguments

--- run.py
+++ run.py
@@ -7,16 +7,21 @@
 #   -d --defaults: provides config.ini with the default config options
 
 import src.financedata as fdat
+import src.setup as setup
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-t','--tinker',action='store_const',const=True,help='open a UI for modifying stored financial data')
-    parser.add_argument('-d','--defaults',action='store_const',const=True,help='reset config.ini to contain the default configuration options')
-    parser.add_argument('-nr','--noreport',action='store_const',const=True,help='override default behaviour to prevent the generation of reports')
+    parser.add_argument('-s','--setup',action='store_const',const=True,help='perform initial setup, and/or initialise accounts, create reports etc.')
+    parser.add_argument('-nr','--noreport',action='store_const',const=True,help='override default behaviour, preventing generation of default reports')
     parser.add_argument('-ni','--noinput',action='store_const',const=True,help='override default behaviour to prevent the reading of drive input files')
+    parser.add_argument('-r','--report',help='generates report with name REPORT')
     args = vars(parser.parse_args())
-    args = [item for item in args if args[item]]
+    args = {item:args[item] for item in args if args[item] is not None}
+
+    if "setup" in args:
+        setup.default_setup_flow()
 
     finance_data = fdat.FinanceData("JR Finances")
     if "defaults" in args:
@@ -26,6 +31,8 @@ if __name__ == "__main__":
         finance_data.read_drive_files()
     if "noreport" not in args:
         finance_data.generate_default_reports()
+    if "report" in args:
+        finance_data.generate_report(args["report"])
     if "tinker" in args:
         finance_data.open_dialogue()
     print("All commands processed. Thank you for running!")
